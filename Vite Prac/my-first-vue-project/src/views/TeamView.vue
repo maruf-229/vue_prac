40914fa47e3ede1c1cf9fc5ba22068272b19cd1b
maruf-brl <79322030+maruf-229@users.noreply.github.com>
2024-03-18 15:54:59 +0600

    aaaaaa

--- Vite Prac/my-first-vue-project/src/views/TeamView.vue
+++ Vite Prac/my-first-vue-project/src/views/TeamView.vue
@@ -8,7 +8,7 @@
     </div>
     <div>
       <div class="inline-flex items-center text-3xl relative">
-        <img src="/smiley.png" alt="" class="mr-2">
+        <img src="/smiley.png" alt="" class="mr-2" style="width: 100px">
         <h3>Smiley Team</h3>
         <div class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2">5</div>
       </div>
